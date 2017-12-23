52ad4b0df453c5ad56326e5b5b67cffd1467e917
ElDainosor <eldainosor@gmail.com>
2017-12-23 03:55:35 +0000

    [2/2] Add 'GoBold' font family
    Required for lockscreen. Also, added in case of adding more fonts.
    
    based on this commit, credits to beanstown and jhenrique
    https://gitlab.com/PixelExperience/vendor_pixelstyle/commit/faa3d8365bc1fa0d885e3e98f1e609f6e0fb85fb

--- config/btlg_main.mk
+++ config/btlg_main.mk
@@ -9,5 +9,14 @@ PRODUCT_PACKAGES += \
     VisualizationWallpapers \
     Jelly
 
+# Fonts to copy
+PRODUCT_COPY_FILES += \
+    vendor/bootleggers/prebuilt/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/bootleggers/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
+
 #Call special ringtones makefile
 include vendor/bootleggers/config/common_audio.mk
