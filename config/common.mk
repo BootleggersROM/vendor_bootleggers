camcory/vendor_androidx

AOSP Keyboard: Fix swipe FC

config/common.mk
@@ -28,6 +28,10 @@ PRODUCT_COPY_FILES +=  \
     vendor/androidx/prebuilt/common/su:root/sbin/su \
     vendor/androidx/prebuilt/common/app/Superuser.apk:system/app/Superuser/Superuser.apk
 
+# Latin IME lib
+PRODUCT_COPY_FILES += \
+    vendor/androidx/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml

BootleggersROM/vendor_bootleggers

config/common.mk
@@ -39,7 +39,8 @@ PRODUCT_COPY_FILES += \
 
 # Copy LatinIME for gesture typing
 PRODUCT_COPY_FILES += \
-    vendor/bootleggers/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+    vendor/bootleggers/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/bootleggers/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
 
 # SELinux filesystem labels
 PRODUCT_COPY_FILES += \