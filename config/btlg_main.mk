a6d1633f1a3585303d169349c972fb26a4351c1f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-04-06 06:27:06 +0000

    vendor: Include GoogleSans Medium font
    Also for future usage

--- config/btlg_main.mk
+++ config/btlg_main.mk
@@ -19,7 +19,8 @@ PRODUCT_COPY_FILES += \
     vendor/bootleggers/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
     vendor/bootleggers/prebuilt/fonts/roadrage/road_rage.ttf:system/fonts/RoadRage-Regular.ttf \
     vendor/bootleggers/prebuilt/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
-    vendor/bootleggers/prebuilt/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf
+    vendor/bootleggers/prebuilt/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
+    vendor/bootleggers/prebuilt/fonts/googlesans/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf
 
 #Call special ringtones makefile
 include vendor/bootleggers/config/common_audio.mk
