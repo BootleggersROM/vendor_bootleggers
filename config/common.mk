08c18715dec10837963159ac5e30d24e93086db0
Dan Pasanen <dan.pasanen@gmail.com>
2018-07-15 12:47:42 +0200

    vendor: add custom backuptools and postinstall script for A/B OTAs
    * A/B OTA devices wont run backuptools in recovery (because they don't
      go in to recovery to do an OTA). In these cases let's use a modified
      version to backup/restore from within android upon postinstall.
    
    * Add backuptool_postinstall.sh which will be run prior to the normal
      postinstall script in order to backup/restore via addon.d scripts.
    
    * This needs to be done in such a manner because we need /postinstall
      mounted rw instead of the ro with context= options which are used for
      the normal postinstall (dexopt) script.
    
    Change-Id: I51511870634dd1ec5388adafddb446f95cc5a950

--- config/common.mk
+++ config/common.mk
@@ -25,6 +25,13 @@ PRODUCT_COPY_FILES += \
     vendor/bootleggers/prebuilt/common/bin/50-bootleg.sh:system/addon.d/50-bootleg.sh \
     vendor/bootleggers/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/bootleggers/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/bootleggers/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/bootleggers/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif
+
 # Backup services whitelist
 PRODUCT_COPY_FILES += \
     vendor/bootleggers/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
