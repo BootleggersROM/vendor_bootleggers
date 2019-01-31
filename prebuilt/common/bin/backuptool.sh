LineageOS/android_vendor_lineage

backuptool: Revert "Temporarily render version check permissive"
This reverts commit fa279c980512c5d25de6bbd4f8d6397a2ae2b252.
Change-Id: Ib3e88ef3ff80bd466c9ec73edc8224713d331657

prebuilt/common/bin/backuptool.sh
@@ -34,10 +34,10 @@ check_prereq() {
 if [ ! -r /system/build.prop ]; then
     return 0
 fi
-# if [ ! grep -q "^ro.lineage.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
-#   echo "Not backing up files from incompatible version: $V"
-#   return 0
-# fi
+if [ ! grep -q "^ro.lineage.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
+  echo "Not backing up files from incompatible version: $V"
+  return 0
+fi
 return 1
 }
 

BootleggersROM/vendor_bootleggers

Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

prebuilt/common/bin/backuptool.sh
@@ -48,10 +48,10 @@ check_prereq() {
 if [ ! -r /system/build.prop ]; then
     return 0
 fi
-# if [ ! grep -q "^ro.bootleggers.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
-#   echo "Not backing up files from incompatible version: $V"
-#   return 0
-# fi
+if [ ! grep -q "^ro.bootleggers.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
+  echo "Not backing up files from incompatible version: $V"
+  return 0
+fi
 return 1
 }
 