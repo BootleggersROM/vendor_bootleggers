PixelExperience/vendor_aosp

vendor: Dynamically add custom APNs
For Example new devices need the new Sprint APNs, so add a new board flag for
them to set that will make the build system add the new APNs to
the APN config XML. If the flag is not set, continue to use the
old APNs.
To add or replace APNs, the custom_apns.py script has been
added. If CUSTOM_APNS_FILE is defined, custom_apns.py
is run and generates a new version of apns-conf.xml.

Change-Id: I7ff12a4342de2a7663b2b66fd627244214a8dc71

prebuilt/common/Android.mk
@@ -1,1 +1,29 @@
 LOCAL_PATH := $(call my-dir)
+
+################################
+# Copies the APN list file into system/etc for the product as apns-conf.xml.
+# In the case where $(CUSTOM_APNS_FILE) is defined, the content of $(CUSTOM_APNS_FILE)
+# is added or replaced to the $(DEFAULT_APNS_FILE).
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := apns-conf.xml
+LOCAL_MODULE_CLASS := ETC
+
+DEFAULT_APNS_FILE := vendor/aosp/prebuilt/common/etc/apns-conf.xml
+
+ifdef CUSTOM_APNS_FILE
+CUSTOM_APNS_SCRIPT := vendor/aosp/tools/custom_apns.py
+FINAL_APNS_FILE := $(local-generated-sources-dir)/apns-conf.xml
+
+$(FINAL_APNS_FILE): PRIVATE_SCRIPT := $(CUSTOM_APNS_SCRIPT)
+$(FINAL_APNS_FILE): PRIVATE_CUSTOM_APNS_FILE := $(CUSTOM_APNS_FILE)
+$(FINAL_APNS_FILE): $(CUSTOM_APNS_SCRIPT) $(DEFAULT_APNS_FILE)
+	rm -f $@
+	python $(PRIVATE_SCRIPT) $@ $(PRIVATE_CUSTOM_APNS_FILE)
+else
+FINAL_APNS_FILE := $(DEFAULT_APNS_FILE)
+endif
+
+LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
+
+include $(BUILD_PREBUILT)

tools/custom_apns.py
@@ -1,0 +1,62 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import sys
+from xml.dom.minidom import parseString
+
+def main(argv):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    original_file = 'vendor/aosp/prebuilt/common/etc/apns-conf.xml'
+
+    if len(argv) == 3:
+        output_file_path = argv[1]
+        custom_override_file = argv[2]
+    else:
+        raise ValueError("Wrong number of arguments %s" % len(argv))
+
+    custom_apn_names = []
+    with open(custom_override_file, 'r') as f:
+        for line in f:
+            xmltree = parseString(line)
+            carrier = xmltree.getElementsByTagName('apn')[0].getAttribute('carrier')
+            custom_apn_names.append(carrier)
+
+    with open(original_file, 'r') as input_file:
+        with open(output_file_path, 'w') as output_file:
+            for line in input_file:
+                writeOriginalLine = True
+                for apn in custom_apn_names:
+                    if apn in line:
+                        with open(custom_override_file, 'r') as custom_file:
+                            for override_line in custom_file:
+                                if apn in override_line:
+                                    output_file.write(override_line)
+                                    writeOriginalLine = False
+                                    custom_apn_names.remove(apn)
+                if writeOriginalLine:
+                    if "</apns>" in line:
+                        if custom_apn_names:
+                            for apn in custom_apn_names:
+                                with open(custom_override_file, 'r') as custom_file:
+                                    for override_line in custom_file:
+                                        if apn in override_line:
+                                            output_file.write(override_line)
+                    output_file.write(line)
+
+if __name__ == '__main__':
+    main(sys.argv)

BootleggersROM/vendor_bootleggers

prebuilt/common/Android.mk
@@ -9,6 +9,34 @@ LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+
+################################
+# Copies the APN list file into system/etc for the product as apns-conf.xml.
+# In the case where $(CUSTOM_APNS_FILE) is defined, the content of $(CUSTOM_APNS_FILE)
+# is added or replaced to the $(DEFAULT_APNS_FILE).
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := apns-conf.xml
+LOCAL_MODULE_CLASS := ETC
+
+DEFAULT_APNS_FILE := vendor/bootleggers/prebuilt/common/etc/apns-conf.xml
+
+ifdef CUSTOM_APNS_FILE
+CUSTOM_APNS_SCRIPT := vendor/bootleggers/tools/custom_apns.py
+FINAL_APNS_FILE := $(local-generated-sources-dir)/apns-conf.xml
+
+$(FINAL_APNS_FILE): PRIVATE_SCRIPT := $(CUSTOM_APNS_SCRIPT)
+$(FINAL_APNS_FILE): PRIVATE_CUSTOM_APNS_FILE := $(CUSTOM_APNS_FILE)
+$(FINAL_APNS_FILE): $(CUSTOM_APNS_SCRIPT) $(DEFAULT_APNS_FILE)
+	rm -f $@
+	python $(PRIVATE_SCRIPT) $@ $(PRIVATE_CUSTOM_APNS_FILE)
+else
+FINAL_APNS_FILE := $(DEFAULT_APNS_FILE)
+endif
+
+LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
+include $(BUILD_PREBUILT)
+
 #
 # Prebuilt APKs
 #

tools/custom_apns.py
@@ -1,0 +1,62 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import sys
+from xml.dom.minidom import parseString
+
+def main(argv):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    original_file = 'vendor/bootleggers/prebuilt/common/etc/apns-conf.xml'
+
+    if len(argv) == 3:
+        output_file_path = argv[1]
+        custom_override_file = argv[2]
+    else:
+        raise ValueError("Wrong number of arguments %s" % len(argv))
+
+    custom_apn_names = []
+    with open(custom_override_file, 'r') as f:
+        for line in f:
+            xmltree = parseString(line)
+            carrier = xmltree.getElementsByTagName('apn')[0].getAttribute('carrier')
+            custom_apn_names.append(carrier)
+
+    with open(original_file, 'r') as input_file:
+        with open(output_file_path, 'w') as output_file:
+            for line in input_file:
+                writeOriginalLine = True
+                for apn in custom_apn_names:
+                    if apn in line:
+                        with open(custom_override_file, 'r') as custom_file:
+                            for override_line in custom_file:
+                                if apn in override_line:
+                                    output_file.write(override_line)
+                                    writeOriginalLine = False
+                                    custom_apn_names.remove(apn)
+                if writeOriginalLine:
+                    if "</apns>" in line:
+                        if custom_apn_names:
+                            for apn in custom_apn_names:
+                                with open(custom_override_file, 'r') as custom_file:
+                                    for override_line in custom_file:
+                                        if apn in override_line:
+                                            output_file.write(override_line)
+                    output_file.write(line)
+
+if __name__ == '__main__':
+    main(sys.argv)