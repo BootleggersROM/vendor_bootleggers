80218880a890e09d5d1f677fe4e11f756136cbb6
El Dainosor <eldainosor@gmail.com>
2019-09-04 01:08:04 +0000

    vendor: Let's push jason body over the river
    4.3: + jason is dead +
    https://nbbeats.bandcamp.com/track/jason-is-dead

--- config/btlg_branding.mk
+++ config/btlg_branding.mk
@@ -3,9 +3,9 @@ PRODUCT_BRAND ?= Bootleggers
 # Versioning System
 # Bootleggers version over here.
 PRODUCT_VERSION_MAJOR = Pie
-PRODUCT_VERSION_MINOR = niceparse.IIIIII
-BOOTLEGGERS_VERSION_NUMBER := 4.2-Stable
-BOOTLEGGERS_SONGCODEURL = http://bit.ly/2R2fQKs
+PRODUCT_VERSION_MINOR = niceparse.+_jason_is_dead_+
+BOOTLEGGERS_VERSION_NUMBER := 4.3-Stable
+BOOTLEGGERS_SONGCODEURL = https://kutt.it/9L7LAv
 BOOTLEGGERS_EPOCH := $(shell date +%s)
 BOOTLEGGERS_POSTFIX := -$(shell date -d @$(BOOTLEGGERS_EPOCH) +"%Y%m%d-%H%M%S")
 
