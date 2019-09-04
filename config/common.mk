PixelExperience/vendor_aosp

vendor: Build Recorder app
Change-Id: I5ea986b54139d7666e99020a66a169e5b66b709b

config/common.mk
@@ -48,6 +48,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Longshot
 
+# Recorder app
+PRODUCT_PACKAGES += \
+    Recorder
+
+# Some permissions
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/privapp-permissions-recorder.xml:system/etc/permissions/privapp-permissions-recorder.xml \
+    vendor/aosp/config/permissions/org.pixelexperience.recorder.xml:system/etc/permissions/org.pixelexperience.recorder.xml
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \

BootleggersROM/vendor_bootleggers

config/common.mk
@@ -73,6 +73,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/bootleggers/config/permissions/privapp-permissions-custom.xml:system/etc/permissions/privapp-permissions-custom.xml
 
+# Some permissions
+PRODUCT_COPY_FILES += \
+    vendor/bootleggers/config/permissions/privapp-permissions-recorder.xml:system/etc/permissions/privapp-permissions-recorder.xml \
+    vendor/bootleggers/config/permissions/org.pixelexperience.recorder.xml:system/etc/permissions/org.pixelexperience.recorder.xml
+
 # TWRP
 ifeq ($(WITH_TWRP),true)
 include vendor/bootleggers/config/twrp.mk