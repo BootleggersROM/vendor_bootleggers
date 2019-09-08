579d6b524313dc55f816fe87a3561b4098f708f8
El Dainosor <eldainosor@gmail.com>
2019-09-08 02:03:16 +0000

    vendor: Migrate to TARGET_BOOTLEG_ARCH
    It'll be more reliable than target_arch, ideal for fixing copying libs

--- config/btlg_main.mk
+++ config/btlg_main.mk
@@ -1,5 +1,5 @@
 # Markup libs
-ifeq ($(TARGET_ARCH),arm64)
+ifeq ($(TARGET_BOOTLEG_ARCH),arm64)
     PRODUCT_COPY_FILES += \
            vendor/bootleggers/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
            vendor/bootleggers/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
