1acd9f6f88459d3f5a3f3026f06125805bdf2a2e
Han Wang <416810799@qq.com>
2019-09-10 13:08:04 +0200

    soong_config: Add new flag for vendor_init
    Change-Id: I383f1de230ecf7b79d78fe701b140bd1bc6795f2

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,10 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-<<<<<<< HEAD
 _contents := $(_contents)    "Bootleggers":{$(newline)
-=======
-_json_contents := $(_json_contents)    "Lineage":{$(newline)
->>>>>>> b49ab91... lineage: soong: _contents -> _json_contents
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
@@ -15,6 +11,7 @@ $(call add_json_bool, Device_support_wait_for_qsee, $(filter true,$(TARGET_KEYMA
 $(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_bool, Needs_text_relocations,                $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str,  Specific_camera_parameter_library,     $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str_omitempty, Target_init_vendor_lib, 		 $(TARGET_INIT_VENDOR_LIB))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str_omitempty, Target_shim_libs,             $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
