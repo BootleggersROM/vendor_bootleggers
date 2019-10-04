3d5ac87672a2472ef7ea5a633ed888f4f8ba6c7a
Rashed Abdel-Tawab <rashed@linux.com>
2019-10-04 03:34:56 +0000

    vendor: Move audio files to product

--- config/common_audio.mk
+++ config/common_audio.mk
@@ -9,21 +9,21 @@ RINGTONE_PATH := vendor/bootleggers/prebuilt/common/media/audio/ringtones
 
 # Alarms
 PRODUCT_COPY_FILES += \
-    $(ALARM_PATH)/BeyondTheDream.ogg:system/media/audio/alarms/BeyondTheDream.ogg \
-    $(ALARM_PATH)/LikeWhat.ogg:system/media/audio/alarms/LikeWhat.ogg
+    $(ALARM_PATH)/BeyondTheDream.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/BeyondTheDream.ogg \
+    $(ALARM_PATH)/LikeWhat.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/LikeWhat.ogg
 
 # Notifications
 PRODUCT_COPY_FILES += \
-    $(NOTIFICATION_PATH)/Auroran.ogg:system/media/audio/notifications/Auroran.ogg \
-    $(NOTIFICATION_PATH)/CyanMail.ogg:system/media/audio/notifications/CyanMail.ogg \
-    $(NOTIFICATION_PATH)/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
-    $(NOTIFICATION_PATH)/Phloreau.ogg:system/media/audio/notifications/Phloreau.ogg
+    $(NOTIFICATION_PATH)/Auroran.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Auroran.ogg \
+    $(NOTIFICATION_PATH)/CyanMail.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/CyanMail.ogg \
+    $(NOTIFICATION_PATH)/CyanMessage.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/CyanMessage.ogg \
+    $(NOTIFICATION_PATH)/Phloreau.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Phloreau.ogg
 
 # Ringtones
 PRODUCT_COPY_FILES += \
-    $(RINGTONE_PATH)/Caraway.ogg:system/media/audio/ringtones/Caraway.ogg \
-    $(RINGTONE_PATH)/Frost.ogg:system/media/audio/ringtones/Frost.ogg \
-    $(RINGTONE_PATH)/Myst.ogg:system/media/audio/ringtones/Myst.ogg
+    $(RINGTONE_PATH)/Caraway.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Caraway.ogg \
+    $(RINGTONE_PATH)/Frost.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Frost.ogg \
+    $(RINGTONE_PATH)/Myst.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Myst.ogg
     
     
 # Default notification/alarm sounds
