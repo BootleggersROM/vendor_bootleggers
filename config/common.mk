b3280ae0de36aa1dc6506bce659ba2cc9e56b504
El Dainosor <eldainosor@gmail.com>
2019-10-04 23:54:06 +0000

    DNM: Switch to TARGET_REQUIRES_TELEPHONY_EXT
    to enforce the building of telephony-ext

--- config/common.mk
+++ config/common.mk
@@ -177,7 +177,7 @@ DEVICE_PACKAGE_OVERLAYS += vendor/bootleggers/overlay/common
 -include vendor/bootleggers/config/partner_gms.mk
 
 ifeq ($(TARGET_PROVIDES_TELEPHONY_EXT),)
-ifneq ($(TARGET_NOT_REQUIRING_TELEPHONY_EXT),true)
+ifeq ($(TARGET_REQUIRES_TELEPHONY_EXT),true)
 include vendor/bootleggers/config/caf_fw.mk
 endif
 endif
