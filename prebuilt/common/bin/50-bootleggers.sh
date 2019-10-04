7c1694ea5424dd190935f2e40a1a781650a918f5
El Dainosor <eldainosor@gmail.com>
2019-10-04 03:00:18 +0000

    update backuptool to 10

--- prebuilt/common/bin/50-bootleggers.sh
+++ prebuilt/common/bin/50-bootleggers.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 #
 # /system/addon.d/50-bootleggers.sh
-# During a Bootleggers 3.0 upgrade, this script backs up /system/etc/hosts,
+# During a Bootleggers Queue upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
