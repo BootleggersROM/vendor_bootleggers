de2de7c76b0c0bc22d7eb0cdbe7e7659b2d428cc
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-10-09 15:30:24 +0200

    vendor: Kill sysinit
    Change-Id: Ia8d46d794ff829f3fc39ff48d57fdb6f9c03262d

--- config/common.mk
+++ config/common.mk
@@ -49,10 +49,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/bootleggers/config/permissions/backup.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/backup.xml
 
-# init.d support
-PRODUCT_COPY_FILES += \
-    vendor/bootleggers/prebuilt/common/bin/sysinit:$(TARGET_COPY_OUT_SYSTEM)/bin/sysinit
-
 # Copy all Bootleggers-specific init rc files
 $(foreach f,$(wildcard vendor/bootleggers/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
