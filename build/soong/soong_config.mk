e0914f28603270348dbc2b76569e3c4478acfd60
merothh <merothh@gmail.com>
2019-10-25 20:47:02 +0530

    soong_config: correct variable name

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "Bootleggers":{$(newline)
+_json_contents := $(_json_contents)    "Bootleggers":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
