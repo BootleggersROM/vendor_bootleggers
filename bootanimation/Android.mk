2a088280a60dff59a50d60f6e2c4640edb505d7f
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-10-27 00:13:04 +0000

    vendor: Rework bootanimation generation
    Change-Id: Ib0b363c3e0545cb0c86dc69dd1a75f82dd777e59
    
    Bootleggers edit: Let's bring back the random bootanim selector and also a var to pick a specific bootanimation

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-#               2017 The LineageOS Project
+#               2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,12 +16,6 @@
 #
 
 ifeq ($(TARGET_SCREEN_WIDTH),)
-ifeq ($(TARGET_SCREEN_HEIGHT),)
-    $(warning TARGET_SCREEN_WIDTH and TARGET_SCREEN_HEIGHT are not defined. For better support, declare the values on your device tree.)
-endif
-endif
-
-ifeq ($(TARGET_SCREEN_WIDTH),)
     $(warning TARGET_SCREEN_WIDTH is not set, using default value: 1080)
     TARGET_SCREEN_WIDTH := 1080
 endif
@@ -30,85 +24,51 @@ ifeq ($(TARGET_SCREEN_HEIGHT),)
     TARGET_SCREEN_HEIGHT := 1920
 endif
 
-ifeq ($(TARGET_BOOTANIMATION_HALF_RES),)
-    TARGET_BOOTANIMATION_HALF_RES := false
-endif
+TARGET_GENERATED_BOOTANIMATION1 := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATION1/bootanimation.zip
+$(TARGET_GENERATED_BOOTANIMATION1): INTERMEDIATES := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATION1
+$(TARGET_GENERATED_BOOTANIMATION1): $(SOONG_ZIP)
+	@echo "Building bootanimation.zip"
+	@rm -rf $(dir $@)
+	@mkdir -p $(dir $@)
+	$(hide) if [ -z $(TARGET_PICK_BOOTANIMATION)]; then \
+      BOOTSELECTED=$$(prebuilts/tools-lineage/common/shuffle/shuf -i 0-9 -n 1); \
+  else \
+      if [ $(TARGET_PICK_BOOTANIMATION) -lt 9]; then \
+          BOOTSELECTED=$(TARGET_PICK_BOOTANIMATION); \
+      else \
+          BOOTSELECTED=$$(prebuilts/tools-lineage/common/shuffle/shuf -i 0-9 -n 1); \
+      fi; \
+  fi; \
+  if [ $(TARGET_SCREEN_HEIGHT) -lt $(TARGET_SCREEN_WIDTH) ]; then \
+	    IMAGEWIDTH=$(TARGET_SCREEN_HEIGHT); \
+	else \
+	    IMAGEWIDTH=$(TARGET_SCREEN_WIDTH); \
+	fi; \
+	IMAGESCALEWIDTH=$$IMAGEWIDTH; \
+	IMAGESCALEHEIGHT=$$(expr $$IMAGESCALEWIDTH / 3); \
+	if [ "$(TARGET_BOOTANIMATION_HALF_RES)" = "true" ]; then \
+	    IMAGEWIDTH="$$(expr "$$IMAGEWIDTH" / 2)"; \
+	fi; \
+	IMAGEHEIGHT=$$(expr $$IMAGEWIDTH / 3); \
+	RESOLUTION="$$IMAGEWIDTH"x"$$IMAGEHEIGHT"; \
+	for part_cnt in 0 1 2; do \
+	    mkdir -p $(INTERMEDIATES)/part$$part_cnt; \
+	done; \
+	tar xfp "vendor/bootleggers/bootanimation/bootanimation$$BOOTSELECTED.tar" --to-command="prebuilts/tools-lineage/${HOST_OS}-x86/bin/convert - -strip -quality 55 -resize $$RESOLUTION^ -colors 250 -gravity center -crop $$RESOLUTION+0+0 +repage \"$(INTERMEDIATES)/\$$TAR_FILENAME\""; \
+	echo "$$IMAGESCALEWIDTH $$IMAGESCALEHEIGHT 25" > $(INTERMEDIATES)/desc.txt; \
+	cat vendor/bootleggers/bootanimation/desc.txt >> $(INTERMEDIATES)/desc.txt
+	$(hide) $(SOONG_ZIP) -L 0 -o $(TARGET_GENERATED_BOOTANIMATION1) -C $(INTERMEDIATES) -D $(INTERMEDIATES)
 
-define build-bootanimation
-    ./vendor/bootleggers/bootanimation/generate-bootanimation.sh \
-    $(TARGET_SCREEN_WIDTH) \
-    $(TARGET_SCREEN_HEIGHT) \
-    $(TARGET_BOOTANIMATION_HALF_RES) \
-    $(TARGET_BOOTANIMATION_BUILDNUMBER) \
-    "$(TARGET_PICK_BOOTANIMATION)"
-endef
-
-TARGET_GENERATED_BOOTANIMATION := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATIONS/1/bootanimation.zip
-TARGET_GENERATED_BOOTANIMATION2 := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATIONS/2/bootanimation2.zip
-TARGET_GENERATED_BOOTANIMATION3 := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATIONS/3/bootanimation3.zip
-
-$(TARGET_GENERATED_BOOTANIMATION):
-	@echo "Building bootanimation"
-	$(eval TARGET_BOOTANIMATION_BUILDNUMBER := 1)
-	$(build-bootanimation)
-
-$(TARGET_GENERATED_BOOTANIMATION2):
-	@echo "Building second bootanimation"
-	$(eval TARGET_BOOTANIMATION_BUILDNUMBER := 2)
-	$(build-bootanimation)
-
-$(TARGET_GENERATED_BOOTANIMATION3):
-	@echo "Building third bootanimation"
-	$(eval TARGET_BOOTANIMATION_BUILDNUMBER := 3)
-	$(build-bootanimation)
-
-ifeq ($(TARGET_BOOTANIMATION),)
-    TARGET_BOOTANIMATION := $(TARGET_GENERATED_BOOTANIMATION)
-endif
-ifeq ($(TARGET_BOOTANIMATION2),)
-    TARGET_BOOTANIMATION2 := $(TARGET_GENERATED_BOOTANIMATION2)
-endif
-ifeq ($(TARGET_BOOTANIMATION3),)
-    TARGET_BOOTANIMATION3 := $(TARGET_GENERATED_BOOTANIMATION3)
-endif
-
-ifeq ($(shell command -v convert),)
-    $(info **********************************************)
-    $(info The boot animation could not be generated as)
-    $(info ImageMagick is not installed in your system.)
-    $(info $(space))
-    $(info Please install ImageMagick from this website:)
-    $(info https://imagemagick.org/script/binary-releases.php)
-    $(info **********************************************)
-    $(error stop)
+ifeq ($(TARGET_BOOTANIMATION1),)
+    TARGET_BOOTANIMATION1 := $(TARGET_GENERATED_BOOTANIMATION1)
 endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := bootanimation.zip
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/media
+
 include $(BUILD_SYSTEM)/base_rules.mk
 
-$(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION)
-	@mkdir -p $(dir $@)
-	@cp $(TARGET_BOOTANIMATION) $@
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := bootanimation2.zip
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/media
-include $(BUILD_SYSTEM)/base_rules.mk
-
-$(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION2)
-	@cp $(TARGET_BOOTANIMATION2) $@
-	@mkdir -p $(dir $@)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := bootanimation3.zip
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/media
-include $(BUILD_SYSTEM)/base_rules.mk
-
-$(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION3)
-	@mkdir -p $(dir $@)
-	@cp $(TARGET_BOOTANIMATION3) $@
+$(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION1)
+	@cp $(TARGET_BOOTANIMATION1) $@
