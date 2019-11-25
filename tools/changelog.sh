9bd9742f02371d2f9f8d25427f6d5f88273f9e4d
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-25 03:55:38 +0200

    vendor: [SQUASH] Update changelog.sh
    Change-Id: I644bf213ec20d6b7efe63d7c19010fa00b47419f

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -5,6 +5,7 @@ dir=$ANDROID_BUILD_TOP
 out=$dir/out/target/product
 
 export Changelog=$PWD/Changelog.txt
+export MANIFEST="${TOP}/.repo/manifests/snippets/bootleggers.xml"
 
 if [ -f $Changelog ];
 then
@@ -13,17 +14,39 @@ fi
 
 touch $Changelog
 
+# Build a list of all repos
+PROJECTPATHS=$(grep "<project" "${MANIFEST}" | sed -n 's/.*path="\([^"]\+\)".*/\1/p')
+
+# Add repos in local manifest for DT changelog
+for lManifest in $TOP/.repo/local_manifests/*; do
+	PROJECTPATHS+=" $(grep "<project" "${lManifest}" | sed -n 's/.*path="\([^"]\+\)".*/\1/p')"
+done
+
 # Print something to build output
 echo ${bldppl}"Generating changelog..."${txtrst}
 
-for i in $(seq 31);
+for i in $(seq 15);
 do
 export After_Date=`date --date="$i days ago" +%F`
 k=$(expr $i - 1)
 export Until_Date=`date --date="$k days ago" +%F`
-echo "" >> $Changelog;
-echo " $Until_Date:"    >> $Changelog;
-repo forall -pc 'git log --after=$After_Date --until=$Until_Date --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=7' >> $Changelog
+
+	# Line with after --- until was too long for a small ListView
+	echo '====================' >> $Changelog;
+	echo "     "$Until_Date     >> $Changelog;
+	echo '====================' >> $Changelog;
+	echo >> $Changelog;
+	
+	# Cycle through every repo to find commits between 2 dates
+	for PROJECTPATH in ${PROJECTPATHS}; do
+		[ ! -d "$TOP/$PROJECTPATH" ] && continue
+		cd "${TOP}/$PROJECTPATH"
+		if ! [[ -z $(git log --after=$After_Date --until=$Until_Date) ]]; then # only echo if there is a change
+			echo "[${PROJECTPATH}]" >> $Changelog
+			git log --after=$After_Date --until=$Until_Date --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=7 >> $Changelog
+			echo >> $Changelog
+		fi
+	done
 echo "" >> $Changelog;
 done
 
