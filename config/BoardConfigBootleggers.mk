cf9df27ffa24402db231d15a8ff05b0cc6bc660f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-10 00:08:02 +0530

    vendor: Allow providing camera server and service
    Change-Id: Icb62bbd394719d2a0a122d6ce9364af1ae0c241f

--- config/BoardConfigBootleggers.mk
+++ config/BoardConfigBootleggers.mk
@@ -7,3 +7,10 @@ endif
 
 
 include vendor/bootleggers/config/BoardConfigSoong.mk
+
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
