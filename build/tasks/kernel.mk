2d38c9e10ed5a3d1fd243e83224932de0f7f7c7f
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-08-01 21:20:23 +0200

    kernel: Add support for module aliases
    * On devices using vendor_overlay it could be useful
      to be able to re-name the installed modules to
      match the ones in vendor
      -> An example is QCOM techpack where default module
         name in kernel doesn't match installed module
         name
    
    Change-Id: If9458bca06218aeb8c91283057c083d909bf5104

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -413,7 +413,11 @@ $(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_CONFIG) $(DEPMOD) $(DTC)
 				$(eval p := $(subst :,$(space),$(s))) \
 				; mv $$(find $$kernel_modules_dir -name $(word 1,$(p))) $$kernel_modules_dir/$(word 2,$(p))); \
 			kernel_release=$$(cat $(KERNEL_RELEASE)) \
-            modules=$$(find $(MODULES_INTERMEDIATES)/lib/modules/$$kernel_release -type f -name '*.ko'); \
+            kernel_modules_dir=$(MODULES_INTERMEDIATES)/lib/modules/$$kernel_release \
+            $(foreach s, $(TARGET_MODULE_ALIASES),\
+                $(eval p := $(subst :,$(space),$(s))) \
+                ; mv $$(find $$kernel_modules_dir -name $(word 1,$(p))) $$kernel_modules_dir/$(word 2,$(p))); \
+            modules=$$(find $$kernel_modules_dir -type f -name '*.ko'); \
 			($(call build-image-kernel-modules-lineage,$$modules,$(KERNEL_MODULES_OUT),$(KERNEL_MODULE_MOUNTPOINT)/,$(KERNEL_DEPMOD_STAGING_DIR),$(BOARD_VENDOR_KERNEL_MODULES_LOAD))); \
 			$(if $(BOOT_KERNEL_MODULES),\
 				vendor_boot_modules=$$(for m in $(BOOT_KERNEL_MODULES); do \
