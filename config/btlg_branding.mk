2da0290e51299dce2c24f6d2a33fc79d4bbfd3df
El Dainosor <eldainosor@gmail.com>
2020-09-20 00:15:25 -0400

    vendor: Pre-R branding

--- config/btlg_branding.mk
+++ config/btlg_branding.mk
@@ -2,9 +2,9 @@ PRODUCT_BRAND ?= Bootleggers
 
 # Versioning System
 # Bootleggers version over here.
-PRODUCT_VERSION_MAJOR = Queue
-PRODUCT_VERSION_MINOR = niceparse.Mayday
-BOOTLEGGERS_VERSION_NUMBER := 5.2-Stable
+PRODUCT_VERSION_MAJOR = Rimbon
+PRODUCT_VERSION_MINOR = niceparse.L
+BOOTLEGGERS_VERSION_NUMBER := 5.7-Alpha
 BOOTLEGGERS_SONGCODEURL = https://cutt.ly/UfgBmik
 BOOTLEGGERS_EPOCH := $(shell date +%s)
 BOOTLEGGERS_POSTFIX := -$(shell date -d @$(BOOTLEGGERS_EPOCH) +"%Y%m%d-%H%M%S")
