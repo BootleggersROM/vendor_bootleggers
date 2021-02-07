1522370975e1049ddb83d1e667b18b8cfc39f2f3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-07 15:37:23 -0300

    vendor: Add back FaceUnlock support
    edit: disable it by default
    
    Change-Id: I361ce913dfb5f1db574b3cf3cdeeeaaca2338ac1
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/btlg_packages.mk
+++ config/btlg_packages.mk
@@ -38,6 +38,17 @@ else
         Launcher3QuickStep
 endif
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= false
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Long Screenshot
 #PRODUCT_PACKAGES += \
 #    StitchImage
