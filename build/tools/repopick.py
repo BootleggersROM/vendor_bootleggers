46dd3e3b293674bfc6a224b5102ec18948a39474
Edgars Cīrulis <edgarsscirulis@gmail.com>
2022-07-28 19:13:01 +0300

    repopick: Link to new gerrit
    Change-Id: I30e40cff84eeef1ff167fdf554a1ad187457d319
    Signed-off-by: Edgars Cīrulis <edgarsscirulis@gmail.com>

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -154,7 +154,7 @@ def fetch_query(remote_url, query):
 
 if __name__ == '__main__':
     # Default to BootleggersROM Gerrit
-    default_gerrit = 'http://review.bootleggersrom.xyz'
+    default_gerrit = 'https://keepthebootleg.lol'
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
         repopick.py is a utility to simplify the process of cherry picking
