e74d256b2e548e054359e30a352886def72b8e14
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:08:53 +0300

    config: Remove BOOTLOADER_MESSAGE_OFFSET
    This hasn't been ported after lineage-18.1
    
    Change-Id: Ie3d798a009a4e0042b767839b39dd6f68695ef2a

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -25,7 +25,6 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += bootleggersGlobalVars
 SOONG_CONFIG_bootleggersGlobalVars += \
     additional_gralloc_10_usage_bits \
-    bootloader_message_offset \
     camera_needs_client_info \
     camera_needs_client_info_lib \
     has_legacy_camera_hal1 \
@@ -61,7 +60,6 @@ SOONG_CONFIG_bootleggersQcomVars_supports_extended_compress_format := $(AUDIO_FE
 SOONG_CONFIG_bootleggersQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Set default values
-BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY ?= 0
@@ -70,7 +68,6 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_bootleggersGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
-SOONG_CONFIG_bootleggersGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_bootleggersGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_bootleggersGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
 SOONG_CONFIG_bootleggersGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
