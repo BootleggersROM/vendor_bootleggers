42d5a2d155da91d739770b4a1cf0a958216272de
El Dainosor <eldainosor@gmail.com>
2022-09-29 15:33:14 -0300

    vendor: Stop copying markup libs
    We're not using markup anymore. Leave that to custom gapps packages.

--- config/btlg_main.mk
+++ config/btlg_main.mk
@@ -1,13 +1,3 @@
-# Markup libs
-ifeq ($(TARGET_BOOTLEG_ARCH),arm64)
-    PRODUCT_COPY_FILES += \
-           vendor/bootleggers/prebuilt/common/lib/libsketchology_native.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsketchology_native.so \
-           vendor/bootleggers/prebuilt/common/lib64/libsketchology_native.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsketchology_native.so
-else
-    PRODUCT_COPY_FILES += \
-           vendor/bootleggers/prebuilt/common/lib/libsketchology_native.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsketchology_native.so
-endif
-
 # Weather
 PRODUCT_COPY_FILES += \
     vendor/bootleggers/prebuilt/common/etc/permissions/com.android.providers.weather.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.providers.weather.xml \
