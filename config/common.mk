3bdc1536b5207068b3e43259a5383b673cdc660f
ralph950412 <ralph950412@gmail.com>
2023-05-10 21:41:16 +0545

    vendor: Enable SystemUIDialog volume panel by default
    Change-Id: I460eeb06d9f1a2d25dae4d8a533285624001441d

--- config/common.mk
+++ config/common.mk
@@ -191,6 +191,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
 
+# Enable SystemUIDialog volume panel
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    sys.fflag.override.settings_volume_panel_in_systemui=true
+
 PRODUCT_PACKAGE_OVERLAYS += vendor/bootleggers/overlay/common
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
